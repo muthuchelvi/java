57eae1c588c9d30b1ac3cb5edb1208ef5df8b958
muthuchelvi <muthuchelvi13@gmail.com>
2025-08-02 11:33:24 +0530

    just a commit

---
+++ java/math.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class math {
+   
+    public static void main(String args[]){
+    Scanner d=new Scanner(System.in);
+    System.out.println("enter first int");
+    int a=d.nextInt();
+    System.out.println("enter 2nd word");
+    String b=d.nextLine();
+     System.out.println("enter 3rd int");
+    int c=d.nextInt();
+
+    }
+}
